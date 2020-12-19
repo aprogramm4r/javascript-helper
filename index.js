671997497160333059bfe6402a0b1028c76317de
aprogramm4r <74236737+aprogramm4r@users.noreply.github.com>
2020-12-19 10:57:22 +0700

    1.0.4

--- index.js
+++ index.js
@@ -22,6 +22,38 @@ function AddEvent(Function,Object,EventType) {
     if (PramFilled) {Object.addEventListener(EventName,Function)}
 }
 
+function ReturnValue(value) {
+    return value
+}
+
+function Package(Name,Description) {
+    var JSON = {
+        "name": Name,
+        "version": "1.0.0",
+        "description": Description,
+        "main": "index.js",
+        "scripts": {
+            "test": "echo \"Error: no test specified\" && exit 1"
+        },
+        "repository": {
+            "type": "git",
+            "url": "git+"
+        },
+        "author": "",
+        "license": "ISC",
+        "bugs": {
+            "url": ""
+        },
+        "homepage": ""
+    }
+    console.log(JSON)
+    console.log("You may copy, paste to a package.json and put the information in the empty one.")
+    console.log("You may also use `$ npm init`")
+    return JSON
+}
+
 module.exports = {
-    AddEvent: AddEvent
+    AddEvent: AddEvent,
+    ReturnValue: ReturnValue,
+    Package: Package
 };
